868cc8360ee634100c5ca931498fafa06c9ed55f
norainu <modeverv@gmail.com>
2018-09-28 15:45:26 +0900

    20180928 15:45:25

--- 1.py
+++ 1.py
@@ -45,22 +45,69 @@ corpus, word_to_id, id_to_word = preprocess(text)
 
 print(word_to_id)
 
+
 def create_co_matrix(corpus, vocab_size, window_size=1):
     corpus_size = len(corpus)
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
-    
     for idx, word_id in enumerate(corpus):
-        for _ in range(1, window_size + 1):
-            left_idx = idx - 1
-            right_idx = idx + 1
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
 
             if left_idx >= 0:
                 left_word_id = corpus[left_idx]
                 co_matrix[word_id, left_word_id] += 1
-            
+
             if right_idx < corpus_size:
                 right_word_id = corpus[right_idx]
-                co_matrix[word_id,right_word_id] += 1
-        
-        return co_matrix
+                co_matrix[word_id, right_word_id] += 1
+    return co_matrix
+
+
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
+
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+vocab_size = len(word_to_id)
+C = create_co_matrix(corpus, vocab_size)
+c0 = C[word_to_id['you']]  # 「you」の単語ベクトル
+c1 = C[word_to_id['i']]  # 「i」の単語ベクトル
+print(c0)
+print(c1)
+print(cos_similarity(c0, c1))
+
+################################
+
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    if query not in word_to_id:
+        print("%s is not found" % query)
+        return
+
+    print("\n[query] " + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    vocab_size = len(id_to_word)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print("%s: %s" % (id_to_word[i], similarity[i]))
+        count += 1
+        if count >= top:
+            return
+
+
+most_similar('you', word_to_id, id_to_word, C, top=5)
+
+###############################
 
