2b4939dde2e3ee4d416d9c123f954118872c4046
modeverv <modeverv@gmail.com>
2018-09-30 10:22:07 +0900

    20180930 10:22:07

--- 1.py
+++ 1.py
@@ -111,3 +111,57 @@ most_similar('you', word_to_id, id_to_word, C, top=5)
 
 ###############################
 
+def ppmi(C, verbose=False, eps=1e-8):
+    M=np.zeros_like(C, dtype=np.float32)
+    N=np.sum(C)
+    S=np.sum(C, axis=0)
+    total=C.shape[0] * C.shape[1]
+    cnt=0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j] * S[i]) + eps)
+            M[i, j] = max(0, pmi)
+            if verbose:
+                cnt += 1
+                if cnt % (total // 100) == 0:
+                    print("%.1f%% done" % (100 * cnt / total))
+
+    return M
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+vocab_size = len(word_to_id)
+C = create_co_matrix(corpus, vocab_size)
+W = ppmi(C)
+np.set_printoptions(precision=3) # 有効桁3桁で表示
+print('covariance matrix')
+print(C)
+print('-'*50)
+print('PPMI')
+print(W)
+
+import sys
+import os
+libr = os.path.dirname(os.path.abspath(__file__)) + '/deep-learning-from-scratch-2'
+sys.path.append(libr)
+#print(sys.path)
+import numpy as np
+import matplotlib.pyplot as plt
+from common.util import preprocess, create_co_matrix, ppmi
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+vocab_size = len(id_to_word)
+C = create_co_matrix(corpus, vocab_size, window_size=1)
+W = ppmi(C)
+
+# SVD
+U, S, V = np.linalg.svd(W)
+
+for word, word_id in word_to_id.items():
+    plt.annotate(word, (U[word_id, 0],U[word_id,1]))
+
+plt.scatter(U[:,0], U[:,1],alpha=0.5)
+plt.show()
+
