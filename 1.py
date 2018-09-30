e0e9d1f2fd15ed7c073c874de449f172a1b83dfd
modeverv <modeverv@gmail.com>
2018-09-30 22:48:46 +0900

    20180930 22:48:46

--- 1.py
+++ 1.py
@@ -188,7 +188,11 @@ from dataset import ptb
 
 window_size = 2
 wordvec_size = 100
-
+try:
+    from sklearn.utils.extmath import randomized_svd
+except ImportError:
+    print("can't import")
+    exit()
 corpus, word_to_id, id_to_word = ptb.load_data('train')
 vocab_size = len(word_to_id)
 print('counting  co-occurrence ...')
@@ -199,6 +203,7 @@ W = ppmi(C, verbose=True)
 print('calculating SVD ...')
 try:
     # truncated SVD (fast!)
+
     from sklearn.utils.extmath import randomized_svd
     U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5,
                              random_state=None)
