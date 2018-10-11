f6c2921022c378a82fdfabac6758e8b88c7961ff
modeverv <modeverv@gmail.com>
2018-10-11 23:10:50 +0900

    20181011 23:10:50

--- 2.py
+++ 2.py
@@ -26,4 +26,39 @@ h = 0.5 * (h0 + h1)
 s = out_layer.forward(h)
 
 print(s)
+################################################
+import sys
+import os
+libr = os.path.dirname(os.path.abspath(__file__)) + '/deep-learning-from-scratch-2'
+sys.path.append(libr)
+import numpy as np
+from common.util import preprocess
+from common.util import convert_one_hot
 
+text = "You say goodbye and I say hello."
+corpus, word_to_id, id_to_word = preprocess(text)
+print(corpus)
+print(id_to_word)
+
+def create_context_target(corpus, window_size=1):
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size,len(corpus) - window_size):
+        cs = []
+        for t in range(-window_size,window_size+1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx+t])
+        contexts.append(cs)
+    return np.array(contexts),np.array(target)
+
+contexts,target = create_context_target(corpus,window_size=1)
+print(contexts)
+
+vocab_size = len(word_to_id)
+target = convert_one_hot(target, vocab_size)
+contexts = convert_one_hot(contexts,vocab_size)
+
+print(target)
+print(contexts)
