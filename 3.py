74c4fae8d3875332767561c8f3aa2a98c1e6a72f
modeverv <modeverv@gmail.com>
2018-10-18 00:55:30 +0900

    20181018 00:55:30

--- 3.py
+++ 3.py
@@ -46,3 +46,22 @@ class SimpleCBOW:
         self.in_layer1.backward(da)
         self.in_layer0.backward(da)
         return None
+
+class Embedding:
+    def __init__(self,W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self,idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self,dout):
+        dW, = self.grads
+        dW[...] = 0
+        #dw[self.idx] = dout
+        np.add.at(dW,self.idx,dout)
+        return None
